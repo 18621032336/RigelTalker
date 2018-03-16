cab33913a3c53cde92e2b2d424545bd9fc43ee77
18621032336 <1351978299@qq.com>
2018-03-16 10:53:11 +0800

    完善伪代码

--- pseudocode.js
+++ pseudocode.js
@@ -1,22 +1,62 @@
+/**
+ * 观察数据
+ */
 class Watcher() {
-  constructor() {
+}
+
+// 光学
+class OpticsWatcher extends Watcher() {
+  constructor(){
+    super();
   }
-  getCatch(){
-    return this.catch;
+  getCatch() {
+    return catchData;
   }
 }
 
-class OpticsWatcher extends Watcher() {
+// 声纳
+class SonarSensor extends Watch() {
+  constructor(){
+    super();
+  }
+  getCatch() {
+    return catchData;
+  }
 }
 
 
 class RigelTalker() {
-  constructor(output, config = {}) {
+  /**
+   * @param output 输出
+   * @param config
+   */
+  constructor(output, config = {level: 1}) {
     if (output isn't Function || output don't exist) {
       throw new Error('method output must be exist')
     }
-    this.output = output;
     
+    this.output = output;
+    this.baseMap = {}; // 基础数据
+    
+    if (level > 0) {
+      let optics = new OpticsWatcher()
+      optics.on('catch', (catchData)={
+        this.merge(catchData)
+      })
+    }
+    
+    if (level > 1) {
+      let optics = new SonarSensor()
+      optics.on('catch', (catchData)={
+        this.merge(catchData)
+      })
+    }
+  }
+  
+  merge(data) {
+    this.baseMap += data;
+    
+    this.output(this.baseMap); // 输出数据
   }
   
   
